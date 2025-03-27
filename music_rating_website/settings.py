1c8b6a23fd4d6f1c5d23d31189148f1977fe27a4
NCHS6 <nickleby.simmonds@gmail.com>
2025-03-27 14:48:21 +0000

    Basic Spotify API added

--- music_rating_website/settings.py
+++ music_rating_website/settings.py
@@ -23,6 +23,9 @@ DATABASES = {
     'default': env.db('DATABASE_URL')
 }
 
+SPOTIFY_CLIENT_ID = env('SPOTIFY_CLIENT_ID')
+SPOTIFY_CLIENT_SECRET = env('YOUR_SPOTIFY_CLIENT_SECRET')
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -71,10 +74,13 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+
             ],
         },
     },
 ]
+
+SESSION_ENGINE = 'django.contrib.sessions.backends.db'
 
 WSGI_APPLICATION = "music_rating_website.wsgi.application"
 
@@ -112,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
