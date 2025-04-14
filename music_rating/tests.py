2e35f75f8e4f6157a4908672dcfed0abba7f5962
Moyin Eyiowuawi <Moyine1@gmail.com>
2025-04-14 01:15:37 +0100

    Changed models to get rid of unnecessary fields, while adding necessary fields such as spotify ID. Also added tests for the new models. Removed tests for API due to the change in views.py

--- music_rating/tests.py
+++ music_rating/tests.py
@@ -1,151 +1,118 @@
 from django.test import TestCase, Client
-from .models import Song, Artist, Album, Single, EP
+from .models import Song, Artist, Album, Single, EP, Rating, RateSystem
 from unittest.mock import patch
 from urllib.parse import urlencode
 from django.conf import settings
 
 
-class ModelTests(TestCase):
-    def test_song_name_and_artist(self):
-        testArtist = Artist.objects.create(artist_name="Wizkid")
-        testSong = Song.objects.create(song_name="Fever")
-        testSong.artist_name.set([testArtist])
+class ArtistModelTest(TestCase):
+    def setUp(self):
+        self.artist = Artist.objects.create(
+            spotify_id="12345",
+            artist_name="Test Artist",
+        )
 
-        self.assertEqual(testSong.song_name, "Fever")
-        self.assertEqual(testSong.artist_name.first().artist_name, "Wizkid")
+    def test_artist_str(self):
+        self.assertEqual(str(self.artist), "Test Artist")
 
-    def test_artist(self):
-        testArtist = Artist.objects.create(artist_name="Smino")
-        testAlbum = Album.objects.create(album_name="Luv 4 Rent")
-        testAlbum.artist_name.set([testArtist])
-        testSong1 = Song.objects.create(song_name="90 Proof", album=testAlbum)
-        testEP = EP.objects.create(ep_name="blkjuptr")
-        testEP.artist_name.set([testArtist])
-        testSong2 = Song.objects.create(song_name="Zoom", ep=testEP)
-        testSingle = Single.objects.create(single_name="Baguetti")
-        testSingle.artist_name.set([testArtist])
-        testSong3 = Song.objects.create(song_name="Baguetti", single=testSingle)
-        self.assertEqual(testSong1.album.album_name, "Luv 4 Rent")
-        self.assertEqual(testSong2.ep.ep_name, "blkjuptr")
-        self.assertEqual(testSong3.single.single_name, "Baguetti")
-        self.assertEqual(testSong1.get_artist_name, "Smino")
-        self.assertEqual(testSong2.get_artist_name, "Smino")
-        self.assertEqual(testSong3.get_artist_name, "Smino")
-        self.assertEqual(testAlbum.songs_album.first().song_name, "90 Proof")
-        self.assertEqual(testEP.songs_ep.first().song_name, "Zoom")
-        self.assertEqual(testSingle.songs_single.first().song_name, "Baguetti")
+    def test_artist_fields(self):
+        self.assertEqual(self.artist.spotify_id, "12345")
+        self.assertEqual(self.artist.artist_name, "Test Artist")
 
-class GetArtistNameTests(TestCase):
-    def test_get_artist_name_album(self):
-        testArtist = Artist.objects.create(artist_name="Wizkid")
-        testAlbum = Album.objects.create(album_name="Made in Lagos")
-        testAlbum.artist_name.set([testArtist])
-        testSong = Song.objects.create(song_name="Grace", album=testAlbum)
-        self.assertEqual(testSong.get_artist_name, "Wizkid")
+class SongModelTest(TestCase):
+    def setUp(self):
+        self.artist1 = Artist.objects.create(spotify_id="12345", artist_name="Artist 1")
+        self.artist2 = Artist.objects.create(spotify_id="67890", artist_name="Artist 2")
+        self.song = Song.objects.create(
+            spotify_id="54321",
+            song_name="Test Song",
+        )
+        self.song.artists.add(self.artist1, self.artist2)
+
+    def test_song_str(self):
+        self.assertEqual(str(self.song), "Test Song")
     
-    def test_get_artist_name_ep(self):
-        testArtist = Artist.objects.create(artist_name="Namani")
-        testEP = EP.objects.create(ep_name="TEXT LANGUAGE")
-        testEP.artist_name.set([testArtist])
-        testSong = Song.objects.create(song_name="WTF", ep=testEP)
-        self.assertEqual(testSong.get_artist_name, "Namani")
+    def test_song_fields(self):
+        self.assertEqual(self.song.spotify_id, "54321")
+        self.assertEqual(self.song.song_name, "Test Song")
+
+    def test_song_get_artist_names(self):
+        self.assertEqual(self.song.get_artist_names, "Artist 1, Artist 2")
+
+class AlbumModelTest(TestCase):
+    def setUp(self):
+        self.artist1 = Artist.objects.create(spotify_id="12345", artist_name="Artist 1")
+        self.artist2 = Artist.objects.create(spotify_id="67890", artist_name="Artist 2")
+        self.album = Album.objects.create(
+            spotify_id="11111",
+            album_name="Test Album",
+        )
+        self.album.artists.add(self.artist1, self.artist2)
+
+    def test_album_str(self):
+        self.assertEqual(str(self.album), "Test Album")
     
-    def test_get_artist_name_single(self):
-        testArtist = Artist.objects.create(artist_name="Davido")
-        testSingle = Single.objects.create(single_name="FEM")
-        testSingle.artist_name.set([testArtist])
-        testSong = Song.objects.create(song_name="FEM", single=testSingle)
-        self.assertEqual(testSong.get_artist_name, "Davido")
+    def test_album_fields(self):
+        self.assertEqual(self.album.spotify_id, "11111")
+        self.assertEqual(self.album.album_name, "Test Album")
+
+    def test_album_get_artist_names(self):
+        self.assertEqual(self.album.get_artist_names, "Artist 1, Artist 2")
+
+class SingleModelTest(TestCase):
+    def setUp(self):
+        self.artist = Artist.objects.create(spotify_id="12345", artist_name="Artist 1")
+        self.single = Single.objects.create(
+            spotify_id="22222",
+            single_name="Test Single",
+        )
+        self.single.artists.add(self.artist)
+
+    def test_single_str(self):
+        self.assertEqual(str(self.single), "Test Single")
     
-    def test_get_artist_name_null(self):
-        testSong = Song.objects.create(song_name="Tweaker")
-        self.assertEqual(testSong.get_artist_name, "Unknown")
+    def test_single_fields(self):
+        self.assertEqual(self.single.spotify_id, "22222")
+        self.assertEqual(self.single.single_name, "Test Single")
 
-class StrMethodTests(TestCase):
-    def test_str_method_song(self):
-        testSong = Song.objects.create(song_name="Fever")
-        self.assertEqual(str(testSong), "Fever")
+    def test_single_get_artist_names(self):
+        self.assertEqual(self.single.get_artist_names, "Artist 1")
+
+class EPModelTest(TestCase):
+    def setUp(self):
+        self.artist = Artist.objects.create(spotify_id="12345", artist_name="Artist 1")
+        self.ep = EP.objects.create(
+            spotify_id="33333",
+            ep_name="Test EP",
+        )
+        self.ep.artists.add(self.artist)
+
+    def test_ep_str(self):
+        self.assertEqual(str(self.ep), "Test EP")
     
-    def test_str_method_album(self):
-        testAlbum = Album.objects.create(album_name="Made in Lagos")
-        self.assertEqual(str(testAlbum), "Made in Lagos")
+    def test_ep_fields(self):
+        self.assertEqual(self.ep.spotify_id, "33333")
+        self.assertEqual(self.ep.ep_name, "Test EP")
+
+    def test_ep_get_artist_names(self):
+        self.assertEqual(self.ep.get_artist_names, "Artist 1")
     
-    def test_str_method_single(self):
-        testSingle = Single.objects.create(single_name="FEM")
-        self.assertEqual(str(testSingle), "FEM")
+class RatingModelTest(TestCase):
+    def setUp(self):
+        self.rating = Rating.objects.create(score=4.5)
+
+    def test_rating_score(self):
+        self.assertEqual(self.rating.score, 4.5)
     
-    def test_str_method_ep(self):
-        testEP = EP.objects.create(ep_name="TEXT LANGUAGE")
-        self.assertEqual(str(testEP), "TEXT LANGUAGE")
+class RateSystemModelTest(TestCase):
+    def setUp(self):
+        self.rate_system = RateSystem.objects.create(
+            name="Custom Rate System",
+            description="A custom rating system",
+        )
 
-class GetAccessTokenTests(TestCase):
-    @patch('music_rating.views.requests.post')
-    def test_get_access_token_success(self, mock_post):
-        # Mock Spotify API response
-        mock_post.return_value.status_code = 200
-        mock_post.return_value.json.return_value = {
-            'access_token': 'mock_access_token',
-            'expires_in': 3600
-        }
-
-        # Simulate a request to the get_access_token function
-        from music_rating.views import get_access_token
-        request = self.client.request().wsgi_request  # Create a mock request object
-        token = get_access_token(request)
-
-        # Assert that the token is correctly retrieved
-        self.assertEqual(token, 'mock_access_token')
-        self.assertIn('access_token', request.session)
-        self.assertIn('token_expiry_time', request.session)
-    
-    @patch('music_rating.views.requests.post')
-    def test_get_access_token_failure(self, mock_post):
-        # Mock Spotify API failure response
-        mock_post.return_value.status_code = 400
-        mock_post.return_value.json.return_value = {
-            'error': 'invalid_client'
-        }
-
-        # Simulate a request to the get_access_token function
-        from music_rating.views import get_access_token
-        request = self.client.request().wsgi_request  # Create a mock request object
-        token = get_access_token(request)
-
-        # Assert that the token is None (failure case)
-        self.assertIsNone(token)
-
-        # Assert that no access token or expiry time is stored in the session
-        self.assertNotIn('access_token', request.session)
-        self.assertNotIn('token_expiry_time', request.session)
-
-class SpotifySearchTests(TestCase):
-    @patch('music_rating.views.requests.get')
-    def test_search_spotify_success(self, mock_get):
-        # Mock Spotify API response
-        mock_get.return_value.status_code = 200
-        mock_get.return_value.json.return_value = {
-            'tracks': {'items': []},
-            'albums': {'items': []},
-            'artists': {'items': []}
-        }
-
-        client = Client()
-        query = urlencode({'query': 'test'})
-        response = client.get(f'/spotify-search/?{query}')
-        self.assertIn('tracks', response.json())
-        self.assertIn('albums', response.json())
-        self.assertIn('artists', response.json())
-    
-    @patch('music_rating.views.requests.get')
-    def test_spotify_search_failure(self, mock_get):
-        # Mock Spotify API failure response
-        mock_get.return_value.status_code = 400
-
-        client = Client()
-        query = urlencode({'query': 'test'})
-        response = client.get(f'/spotify-search/?{query}')  
-        self.assertEqual(response.status_code, 400)
-        self.assertIn('Failed to fetch data from Spotify', response.json()['error'])
+    def test_rate_system_str(self):
+        self.assertEqual(str(self.rate_system), "Custom Rate System")
 
 class TemplateViewTests(TestCase):
     def test_entrypage_view(self):
@@ -187,7 +154,7 @@ class TemplateViewTests(TestCase):
     def test_album_detail_view(self):
         testArtist = Artist.objects.create(artist_name="Wizkid")
         testAlbum = Album.objects.create(album_name="Made in Lagos")
-        testAlbum.artist_name.set([testArtist])
+        testAlbum.artists.set([testArtist])
         testSong = Song.objects.create(song_name="Grace", album=testAlbum)
         
         client = Client()
@@ -195,3 +162,4 @@ class TemplateViewTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'music_rating/album_detail.html')
 
+#TODO: Need to write tests for the updated models page
