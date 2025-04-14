402a1add55f7fe370244fb0eef71ad3327c311fb
Moyin Eyiowuawi <Moyine1@gmail.com>
2025-04-14 01:15:37 +0100

    Changed models to get rid of unnecessary fields, while adding necessary fields such as spotify ID. Also added tests for the new models. Removed tests for API due to the change in views.py

--- music_rating/models.py
+++ music_rating/models.py
@@ -15,10 +15,9 @@ class Rating(models.Model):
 
 
 class Song(models.Model):
+    spotify_id = models.CharField(max_length=50, unique=True, db_index=True, default="")
     song_name = models.CharField(max_length=200)
-    no_of_streams = models.IntegerField(null=True)
-    no_of_minutes = models.IntegerField(null=True)
-    artist_name = models.ManyToManyField("Artist", blank = True)
+    artists = models.ManyToManyField("Artist", blank = True)
     feat_artists = models.ManyToManyField("Artist", related_name="featured_songs", blank=True)
     rating = models.ForeignKey(Rating, on_delete=models.SET_NULL, null = True, blank = True)
     optional_writing = models.TextField(blank = True, null=True)
@@ -29,20 +28,15 @@ class Song(models.Model):
     def __str__(self):
         return self.song_name
 
-    
     @property
-    def get_artist_name(self):
-        if self.album and self.album.artist_name.exists():
-            return self.album.artist_name.first().artist_name
-        elif self.ep and self.ep.artist_name.exists():
-            return self.ep.artist_name.first().artist_name
-        elif self.single and self.single.artist_name.exists():
-            return self.single.artist_name.first().artist_name
-        return "Unknown"
+    def get_artist_names(self):
+        # Return a comma-separated list of artist names
+        return ", ".join([artist.artist_name for artist in self.artists.all()])
     
 class Album(models.Model):
+    spotify_id = models.CharField(max_length=50, unique=True, db_index=True, default="")
     album_name = models.CharField(max_length=200)
-    artist_name = models.ManyToManyField("Artist")
+    artists = models.ManyToManyField("Artist")
     songs = models.ManyToManyField(Song, related_name="album_songs")
     album_rating = models.ForeignKey(Rating, on_delete=models.SET_NULL, blank=True, null=True)
     optional_writing = models.TextField(blank = True, null = True)
@@ -50,9 +44,15 @@ class Album(models.Model):
     def __str__(self):
         return self.album_name
 
+    @property
+    def get_artist_names(self):
+        # Return a comma-separated list of artist names
+        return ", ".join([artist.artist_name for artist in self.artists.all()])
+
 class Single(models.Model):
+    spotify_id = models.CharField(max_length=50, unique=True, db_index=True, default="")
     single_name = models.CharField(max_length=200)
-    artist_name = models.ManyToManyField("Artist")
+    artists = models.ManyToManyField("Artist")
     songs = models.ManyToManyField(Song, related_name="single_songs")
     single_rating = models.ForeignKey(Rating, on_delete = models.SET_NULL, blank = True, null = True)
     optional_writing = models.TextField(blank = True, null = True)
@@ -60,9 +60,15 @@ class Single(models.Model):
     def __str__(self):
         return self.single_name
 
+    @property
+    def get_artist_names(self):
+        # Return a comma-separated list of artist names
+        return ", ".join([artist.artist_name for artist in self.artists.all()])
+
 class EP(models.Model):
+    spotify_id = models.CharField(max_length=50, unique=True, db_index=True, default="")
     ep_name = models.CharField(max_length=200)
-    artist_name = models.ManyToManyField("Artist")
+    artists = models.ManyToManyField("Artist")
     songs = models.ManyToManyField(Song, related_name="ep_songs")
     ep_rating = models.ForeignKey(Rating, on_delete=models.SET_NULL, blank = True, null = True)
     optional_writing = models.TextField(blank = True, null = True)
@@ -70,11 +76,14 @@ class EP(models.Model):
     def __str__(self):
         return self.ep_name
 
+    @property
+    def get_artist_names(self):
+        # Return a comma-separated list of artist names
+        return ", ".join([artist.artist_name for artist in self.artists.all()])
+
 class Artist(models.Model):
-    artist_name = models.CharField(max_length=255, null=True) #Had to allow null = True so I could migrate, shouldn't allow artists without names tho.
-    albums = models.ManyToManyField(Album, blank = True)
-    singles = models.ManyToManyField(Single, blank = True)
-    eps = models.ManyToManyField(EP, blank = True)
+    spotify_id = models.CharField(max_length=50, unique=True, db_index=True, default="")
+    artist_name = models.CharField(max_length=255, null=False)
     rate_system = models.ForeignKey(RateSystem, on_delete=models.CASCADE, default=1)
     artist_rating = models.ForeignKey(Rating, on_delete=models.SET_NULL, blank = True, null = True)
     optional_writing = models.TextField(blank = True, null = True)
