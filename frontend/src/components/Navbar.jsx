0e45b6bb7ec9eed19189c21ea70f6b543f99f26f
Moyin Eyiowuawi <Moyine1@gmail.com>
2025-08-31 16:02:43 +0100

    Added react to this branch and rendered the homepage with both the searchbar and nav bar.

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Navbar(){ 
+    return (
+        <nav>
+            <a href="/homepage">Home</a>
+            <a href="/albums">Albums</a>
+            <a href="/artists">Artists</a>
+            <a href="/community">Community Page</a>
+            <a href="/entries">Rating Entries</a>
+            <a href="/songs">Songs</a>
+        </nav>
+    );
+}
