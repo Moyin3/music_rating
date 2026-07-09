a9347e6a9ce4ce27ba645b2cbb5add98c7104254
Moyin Eyiowuawi <Moyine1@gmail.com>
2026-07-09 17:29:14 +0100

    Added a proxy for the spotify search url, so it gets processed against the backend server

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from "react-router";
 import './index.css'
 import App from './App'
-import { BrowserRouter } from 'react-router'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
