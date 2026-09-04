bf02cd27382913062941751afc0d5565f810a4af
Moyin Eyiowuawi <Moyine1@gmail.com>
2026-09-04 16:30:45 +0100

    After login redirect, once a successful login has been completed users are returned to the page they had originally been trying to access

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -1,7 +1,7 @@
 import { useState, useContext } from 'react';
 import LoginCheck from '../functions/LoginCheck';
 import post from '../functions/post';
-import { useLocation } from "react-router";
+import { useLocation, useNavigate } from "react-router";
 import AuthContext from '../functions/AuthContext';
 
 const LoginForm = () => {
@@ -10,19 +10,21 @@ const LoginForm = () => {
     const [password, setPassword] = useState('');
     const [email, setEmail] = useState('');
     const { user, setUser } = useContext(AuthContext);
-    
-
+    const location = useLocation();
+    const navigate = useNavigate();
+    // This is data that has been given for the original route, but ended up in the redirect 
+    const priorData = location.state.priorData.Data; 
     const handleSubmit = async (event) => {
         event.preventDefault();
     
     const data = { username, email, password };
 
     const response = await post({"url": "http://127.0.0.1:8000/api/dj-rest-auth/login/", "data": data})
-
+    
     if (response.status == "success"){
         console.log(response)
         setUser(await LoginCheck());
-
+        navigate(location.state?.priorData?.location?.pathname || "/", {replace: true, state: {priorData}});
     }else if (response.status == "error: request failed"){
         //TODO: Do this later
     }else{
--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,9 +1,12 @@
-import { Navigate, Outlet } from "react-router";
+import { Navigate, Outlet, useLocation } from "react-router";
 
 const ProtectedRoute = (props) =>{
+const location = useLocation();
+const Data = location.state;
+console.log("Is it getting here?", Data)
     return (
         props.isAuthenticated ?
-        <Outlet /> : <Navigate to = "/login" />
+        <Outlet /> : <Navigate to = "/login" state = {{priorData: {location, Data}}}  />
     )
 }
 
