dbbec8d46f4047e1469e8cbe7aeb74436a054ff3
Moyin Eyiowuawi <Moyine1@gmail.com>
2026-09-11 19:02:27 +0100

    email verification workflow complete

--- frontend/src/pages/VerifyEmailPage.jsx
+++ frontend/src/pages/VerifyEmailPage.jsx
@@ -1,21 +1,31 @@
 import { useParams } from "react-router";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import post from "../functions/post";
 
 const VerifyEmailPage = () =>{
 let params = useParams();
 const data = {"key":params.key}
+const [result, setResult] = useState(false);
+
 
 const VerifyEmail = async(event) =>{
 const response = await post({"url": "/api/auth/registration/verify-email/", "data": data});
 console.log(data)
+console.log("What is happening to verification", response);
+return response;
 }
 useEffect(()=>{
     const Verify = async () =>{
-        await VerifyEmail();
+       let value = await VerifyEmail();
+        setResult(value);
     }
     Verify();
 }, [])
+return(
+    result.status == "success" &&(
+        <h1>Email Verified</h1>
+    )
+)
 }
 
 export default VerifyEmailPage; 
