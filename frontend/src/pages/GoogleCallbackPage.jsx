6d513d5334e5632d68f15efd83acbc520a83c92f
Moyin Eyiowuawi <Moyine1@gmail.com>
2026-09-17 20:56:43 +0100

    quick Gmail login issue fix

--- frontend/src/pages/GoogleCallbackPage.jsx
+++ frontend/src/pages/GoogleCallbackPage.jsx
@@ -1,17 +1,21 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useContext } from "react";
 import { handleGoogleCallback } from "../functions/GoogleAuth";
 import NavigationBar from "../components/Navigationbar";
 import { useNavigate } from "react-router";
-
+import LoginCheck from "../functions/LoginCheck";
+import AuthContext from '../functions/AuthContext';
 
 const GoogleCallbackPage = () =>{
 const navigate = useNavigate();
 const calledRef = useRef(false);
+const { user, setUser } = useContext(AuthContext);
+
 useEffect(()=>{
     if (calledRef.current) return;
     calledRef.current = true;
-    handleGoogleCallback().then((result) => {
+    handleGoogleCallback().then(async (result) => {
         if (result.success) {
+            setUser(await LoginCheck());
             navigate('/')
         } else{
             navigate('/login?error=1');
